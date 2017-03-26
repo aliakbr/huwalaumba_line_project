d07e980af489181bee0e4774c0e4afc6ee4d728f
Ali Akbar <aliraal12@gmail.com>
2017-03-27 03:24:45 +0700

    Add lib

--- src/com/Huwalaumba/LineBotClient/Main.java
+++ src/com/Huwalaumba/LineBotClient/Main.java
@@ -1,8 +1,15 @@
 package com.Huwalaumba.LineBotClient;
 
+
+import com.linecorp.bot.client.LineMessagingService;
+import com.linecorp.bot.client.LineMessagingServiceBuilder;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	    // write your code here
+        LineMessagingService client = LineMessagingServiceBuilder.create("T3+2tPn1Tsvr2H4maVo3naXVkwlMyIKtA9uWOiNGX/f37i" +
+                "62fKCdU1vi/vD9OGs4cFYS2U7zi/x850BrBfWIEqpeSrMOLy4JhlDmBEN3GJo+WyrV3R56ZS3Wq114L8RYNjPqPm0WSMH7KYGTLMqxI" +
+                "AdB04t89/1O/w1cDnyilFU=").build();
     }
 }
